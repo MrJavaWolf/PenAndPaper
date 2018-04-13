b6c185e00333be48aa1bb3c6d5b2538acb4ae46f
Przemyslaw Krowinski <hello@krowinski.com>
2018-04-14 00:53:58 +0200

    Add basic character selection

--- Pen And Paper/Assets/Scripts/CharacterSelect.cs
+++ Pen And Paper/Assets/Scripts/CharacterSelect.cs
@@ -3,14 +3,29 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CharacterSelect : MonoBehaviour {
+	public int currentSelection = 1;
 
 	// Use this for initialization
 	void Start () {
 		
 	}
+
+	void Left() {
+      
+	}
+
+	void updateSelected (int newSelection, bool isOn) {
+      GameObject box = transform.Find("Character " + newSelection.ToString()).gameObject;
+	  CharacterBox characterBox = box.GetComponent<CharacterBox>();
+	  characterBox.Toggle(isOn);
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyUp(KeyCode.RightArrow)) {
+			this.updateSelected(this.currentSelection, false);
+			this.currentSelection = (this.currentSelection) % 6 + 1;
+			this.updateSelected(this.currentSelection, true);
+		}
 	}
 }
