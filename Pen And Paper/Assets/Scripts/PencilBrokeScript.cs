a8e2cb35ff8444a77f654aecb909c251fccc3afe
Jens Larsen <jepl@dictus.dk>
2018-04-14 20:00:08 +0200

    Added: Paper Punctured, Paper Ripped and Pencil Broke visual scernarioes

---
+++ Pen And Paper/Assets/Scripts/PencilBrokeScript.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PencilBrokeScript : Singleton<PencilBrokeScript>
+{
+
+    public Vector3 CameraOffset = new Vector3(2, 3, -5);
+
+    public void Play(Action onDone)
+    {
+        StartCoroutine(PlaySequence(onDone));
+    }
+
+    private IEnumerator PlaySequence(Action onDone)
+    {
+        var cameraPosition = PenController.Instance.GetTipPosition() + CameraOffset;
+        Camera.main.transform.position = cameraPosition;
+        Camera.main.transform.LookAt(PenController.Instance.GetTipPosition());
+        yield return new WaitForSeconds(4);
+        onDone();
+    }
+}
