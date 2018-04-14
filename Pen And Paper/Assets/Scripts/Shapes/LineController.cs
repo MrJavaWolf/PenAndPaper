ce281b2f0bfddc1a2546f6ede011ff3012a89f14
Peter Witt <peter.witt.joergensen@gmail.com>
2018-04-14 11:57:05 +0200

    Work on drawing

---
+++ Pen And Paper/Assets/Scripts/Shapes/LineController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LineController : ShapeController
+{
+    public List<Transform> positions = new List<Transform>();
+    public float Closeness = .2f;
+
+    protected override bool IsShapeFilled()
+    {
+        List<float> distances = new List<float>();
+
+        foreach (var item in positions)
+        {
+            float closest = 100;
+            foreach (var data in recordedPositions)
+            {
+                var dist = Vector2.Distance(item.position, data.Pos);
+                if (dist < closest)
+                    closest = dist;
+            }
+
+            distances.Add(closest);
+        }
+
+        var avg = distances.Average();
+        Debug.Log(avg);
+
+        if (avg < Closeness)
+            return true;
+
+        return false;
+    }
+}
