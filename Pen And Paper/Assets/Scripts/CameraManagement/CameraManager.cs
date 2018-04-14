b8e4c9f278218859f15aa6526e7839af48db476a
Peter Witt <peter.witt.joergensen@gmail.com>
2018-04-14 17:29:03 +0200

    Added camera angles
    Added dotween

--- Pen And Paper/Assets/Scripts/CameraManagement/CameraManager.cs
+++ Pen And Paper/Assets/Scripts/CameraManagement/CameraManager.cs
@@ -6,6 +6,8 @@ public class CameraManager : Singleton<CameraManager>
 {
     private List<CameraPoint> CamPoints = new List<CameraPoint>();
 
+    private int lastIndex;
+
     private void Start()
     {
         foreach (var camPoint in FindObjectsOfType<CameraPoint>())
@@ -13,6 +15,25 @@ public class CameraManager : Singleton<CameraManager>
             CamPoints.Add(camPoint);
         }
 
-        CamPoints[Random.Range(0, CamPoints.Count - 1)].MountCamera(Camera.main);
+        lastIndex = Random.Range(0, CamPoints.Count - 1);
+        CamPoints[lastIndex].MountCamera(Camera.main);
+    }
+
+    public void NextCameraAngle()
+    {
+        if (CamPoints.Count < 1)
+        {
+            CamPoints[Random.Range(0, CamPoints.Count - 1)].MountCamera(Camera.main);
+            return;
+        }
+
+        int nextIndex;
+
+        do
+            nextIndex = Random.Range(0, CamPoints.Count);
+        while (nextIndex == lastIndex);
+
+        lastIndex = nextIndex;
+        CamPoints[nextIndex].MountCamera(Camera.main);
     }
 }
--- Pen And Paper/Assets/Scripts/CameraManagement/CameraPoint.cs
+++ Pen And Paper/Assets/Scripts/CameraManagement/CameraPoint.cs
@@ -5,11 +5,29 @@ using UnityEngine;
 public class CameraPoint : MonoBehaviour
 {
     public Transform lookPoint;
+    public float Duration = 5;
+    private float timer;
+    private bool Attached;
 
     public void MountCamera(Camera cam)
     {
         cam.transform.position = transform.position;
         cam.transform.SetParent(transform);
         cam.transform.LookAt(lookPoint);
+
+        timer = Duration;
+        Attached = true;
+    }
+
+    private void Update()
+    {
+        if (timer < 0)
+        {
+            CameraManager.Instance.NextCameraAngle();
+            timer = Duration;
+            Attached = false;
+        }
+        else if (Attached)
+            timer -= Time.deltaTime;
     }
 }
