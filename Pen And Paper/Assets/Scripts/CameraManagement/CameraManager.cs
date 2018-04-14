72cf6f5f159c1be0480f4f19260fa95824fc8966
Peter Witt <peter.witt.joergensen@gmail.com>
2018-04-14 16:55:59 +0200

    New Camera Angle

---
+++ Pen And Paper/Assets/Scripts/CameraManagement/CameraManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : Singleton<CameraManager>
+{
+    private List<CameraPoint> CamPoints = new List<CameraPoint>();
+
+    private void Start()
+    {
+        foreach (var camPoint in FindObjectsOfType<CameraPoint>())
+        {
+            CamPoints.Add(camPoint);
+        }
+
+        CamPoints[Random.Range(0, CamPoints.Count - 1)].MountCamera(Camera.main);
+    }
+}
---
+++ Pen And Paper/Assets/Scripts/CameraManagement/CameraPoint.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPoint : MonoBehaviour
+{
+    public Transform lookPoint;
+
+    public void MountCamera(Camera cam)
+    {
+        cam.transform.position = transform.position;
+        cam.transform.SetParent(transform);
+        cam.transform.LookAt(lookPoint);
+    }
+}
