a5071edb28fbacf6309c80cd7290cb48f01afced
Jens Larsen <jepl@dictus.dk>
2018-04-15 00:27:23 +0200

    Updated: Fintuned the Pencil Break animation

--- Pen And Paper/Assets/Scripts/PencilBrokeScript.cs
+++ Pen And Paper/Assets/Scripts/PencilBrokeScript.cs
@@ -12,6 +12,7 @@ public class PencilBrokeScript : Singleton<PencilBrokeScript>
 
     public Vector3 CameraOffsetPencilView = new Vector3(2, 3, -5);
 
+    public Transform Pen;
     public Transform PenTip;
     public Transform[] PenParts;
     public AnimationCurve PartsFlyCurve;
@@ -26,27 +27,49 @@ public class PencilBrokeScript : Singleton<PencilBrokeScript>
         PlayCamera.Instance.transform.position = PenTip.position + CameraOffsetTipView;
         PlayCamera.Instance.transform.LookAt(PenTip.position);
 
-       // CameraShaker.Instance.ShakeOnce(1, 5, 2, 0.1f);
-        yield return new WaitForSeconds(2.5f);
+        Pen.transform.position = new Vector3(Pen.transform.position.x, Pen.transform.position.y, -3f);
+        Pen.DOMove(Pen.transform.position + Pen.transform.forward * 0.3f, 0.2f);
+        yield return new WaitForSeconds(0.2f);
 
-        PenTip.DOMove(PenTip.position + PenTip.up * 0.1f, 0.05f);
-        PenTip.DORotateQuaternion(PenTip.rotation * Quaternion.Euler(new Vector3(-125, 0, 0)), 0.05f);
+        PenTip.DOMove(PenTip.position + Vector3.up * 0.2f - PenTip.transform.forward * 0.2f, 0.1f)
+            .OnComplete(() =>
+            {
+                PenTip.DOMove(PenTip.position + Vector3.up * 0.3f, 7);
+            });
 
-        yield return new WaitForSeconds(1.5f);
+        PenTip.DORotateQuaternion(PenTip.rotation * Quaternion.Euler(new Vector3(-90, 0, 0)), 0.1f)
+            .OnComplete(() =>
+            {
+                PenTip.DORotateQuaternion(PenTip.rotation * Quaternion.Euler(new Vector3(-360, 50, -50)), 7);
+            });
+
+        yield return new WaitForSeconds(0.6f);
 
         PlayCamera.Instance.transform.position = PenTip.position + CameraOffsetPencilView;
         PlayCamera.Instance.transform.LookAt(PenTip.position);
 
-        yield return new WaitForSeconds(1);
-        foreach (var penPart in PenParts)
+        yield return new WaitForSeconds(0.6f);
+        for (int i = 0; i < PenParts.Length; i++)
         {
+            var penPart = PenParts[i];
+
+            Vector3 sideDirections = UnityEngine.Random.insideUnitCircle.normalized;
+            Vector3 forwardDirection = penPart.forward;
+            var direction = Vector3.Lerp(sideDirections, forwardDirection, i / (PenParts.Length - 1));
             penPart.DOMove(penPart.position +
-                penPart.up * UnityEngine.Random.Range(-0.3f, 0.3f) +
-                penPart.forward * UnityEngine.Random.Range(-0.3f, 0.3f) +
-                penPart.right * UnityEngine.Random.Range(-0.3f, 0.3f), 0.1f);
+               penPart.up * direction.y +
+               penPart.forward * direction.z +
+               penPart.right * direction.x, 0.1f)
+            .OnComplete(() =>
+            {
+                penPart.DOMove(penPart.position +
+                    penPart.up * direction.y +
+                    penPart.forward * direction.z +
+                    penPart.right * direction.x, 7f);
+            });
         }
 
-        yield return new WaitForSeconds(4);
+        yield return new WaitForSeconds(5);
         onDone();
     }
 }
