a1ec3facf52b0222f25813a44410465f64aa1465
Jens Larsen <jepl@dictus.dk>
2018-04-14 10:12:38 +0200

    Added: Can control the Paper and Pencil with the controller inputs

---
+++ Pen And Paper/Assets/Scripts/PaperController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaperController : MonoBehaviour
+{
+    public float MovementSpeed = 2;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        var input = InputController.Instance.GetXBoxInput();
+        var movement = input.LeftStick.x * transform.right * MovementSpeed * Time.deltaTime +
+            input.LeftStick.y * transform.up * MovementSpeed * Time.deltaTime;
+        transform.position += movement;
+    }
+}
--- Pen And Paper/Assets/Scripts/PenController.cs
+++ Pen And Paper/Assets/Scripts/PenController.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class PenController : Singleton<PenController>
 {
     public GameObject Tip;
+    public float MovementSpeed = 2;
 
     public Vector3 GetTipPosition()
     {
         return Tip.transform.position;
+    }
+
+    public void Update()
+    {
+        var input = InputController.Instance.GetPs4Input();
+        var movement = input.LeftStick.x * transform.forward * MovementSpeed * Time.deltaTime +
+            input.LeftStick.y * transform.up * MovementSpeed * Time.deltaTime;
+        transform.position += movement;
     }
 }
