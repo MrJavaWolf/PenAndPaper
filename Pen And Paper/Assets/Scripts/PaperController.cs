41932dd90e7118c7f508670e9f0fb830de24d185
Jens Larsen <jepl@dictus.dk>
2018-04-14 12:44:19 +0200

    Updated: Paper prefab

--- Pen And Paper/Assets/Scripts/PaperController.cs
+++ Pen And Paper/Assets/Scripts/PaperController.cs
@@ -5,14 +5,37 @@ using UnityEngine;
 public class PaperController : MonoBehaviour
 {
     public float MovementSpeed = 2;
+    public HandObject LeftHand;
+    public HandObject RightHand;
+    public Transform Paper;
 
+    
 
     // Update is called once per frame
     void Update()
     {
         var input = InputController.Instance.GetXBoxInput();
+        var leftHandMovement = input.LeftStick.x * LeftHand.Hand.right * MovementSpeed * Time.deltaTime +
+            input.LeftStick.y * LeftHand.Hand.up * MovementSpeed * Time.deltaTime;
+
         var movement = input.LeftStick.x * transform.right * MovementSpeed * Time.deltaTime +
             input.LeftStick.y * transform.up * MovementSpeed * Time.deltaTime;
         transform.position += movement;
+
+        
+    }
+
+    private void UpdateHand(HandObject hand)
+    {
+
+    }
+
+
+    public struct HandObject
+    {
+        public Transform Hand;
+        public float UserInputVerticalSpeed;
+        public float BaseVerticalSpeed;
+        public float VerticalShakiness;
     }
 }
