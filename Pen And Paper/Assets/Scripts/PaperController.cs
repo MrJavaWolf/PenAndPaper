320c8f2ea5db3d9e693907704b8b0eaca48b819a
Jens Larsen <jepl@dictus.dk>
2018-04-14 15:04:03 +0200

    Added: PlaySceneController to handle win / lost conditions

--- Pen And Paper/Assets/Scripts/PaperController.cs
+++ Pen And Paper/Assets/Scripts/PaperController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PaperController : MonoBehaviour
+public class PaperController : Singleton<PaperController>
 {
     public HandObject LeftHand;
     public HandObject RightHand;
---
+++ Pen And Paper/Assets/Scripts/PlaySceneController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlaySceneController : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        PaperController.Instance.OnPaperRip += Instance_OnPaperRip;
+        ShapeManager.Instance.PaperPunctured += Instance_PaperPunctured;
+        ShapeManager.Instance.GameEnded += Instance_GameEnded;
+    }
+
+    private void Instance_GameEnded()
+    {
+        SceneManager.LoadScene("Finished Won");
+    }
+
+    private void Instance_PaperPunctured()
+    {
+        SceneManager.LoadScene("Finished Lost");
+    }
+
+    private void Instance_OnPaperRip(object sender, System.EventArgs e)
+    {
+        SceneManager.LoadScene("Finished Lost");
+
+    }
+}
