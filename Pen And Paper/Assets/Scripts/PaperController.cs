bffd7e112329b27aa444af9fa4fe3a887f97c3ee
Jens Larsen <jepl@dictus.dk>
2018-04-14 14:21:37 +0200

    Updated: Paper controlls, the paper rotates now

--- Pen And Paper/Assets/Scripts/PaperController.cs
+++ Pen And Paper/Assets/Scripts/PaperController.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 
 public class PaperController : MonoBehaviour
 {
-    public float MovementSpeed = 2;
     public HandObject LeftHand;
     public HandObject RightHand;
     public Transform Paper;
@@ -13,7 +12,7 @@ public class PaperController : MonoBehaviour
     /// <summary>
     /// How far the hands are allowed to be vertially compared to eachother, before the paper rips. 
     /// </summary>
-    public float PaperRipDistance = 2;
+    public float PaperRipDistance = 3;
 
     public event EventHandler OnPaperRip;
 
@@ -21,17 +20,17 @@ public class PaperController : MonoBehaviour
     void Update()
     {
         var input = InputController.Instance.GetXBoxInput();
-        //UpdateHand(LeftHand, input.LeftStick);
-        //UpdateHand(RightHand, input.RightStick);
-        //if(WillPaperRip())
-        //{
-        //    if (OnPaperRip != null) OnPaperRip(this, EventArgs.Empty);
-        //    Debug.Log("Paper RIP");
-        //}
-        //else
-        //{
-        //    UpdatePaperRotation();
-        //}
+        UpdateHand(LeftHand, input.LeftStick);
+        UpdateHand(RightHand, input.RightStick);
+        if (WillPaperRip())
+        {
+            if (OnPaperRip != null) OnPaperRip(this, EventArgs.Empty);
+            Debug.Log("Paper RIP");
+        }
+        else
+        {
+            UpdatePaperRotation();
+        }
     }
 
     private bool WillPaperRip()
@@ -41,14 +40,18 @@ public class PaperController : MonoBehaviour
 
     private void UpdatePaperRotation()
     {
+        var direction = LeftHand.Hand.position - RightHand.Hand.position;
+        Quaternion rotation = Quaternion.LookRotation(direction, Vector3.up);
+        rotation *= Quaternion.Euler(0, 90, 0);
+        Paper.transform.rotation = rotation;
 
     }
 
     private void UpdateHand(HandObject hand, Vector2 userInput)
     {
-        var handMovement = 
-            //userInput.x * hand.Hand.right * MovementSpeed * Time.deltaTime +
-            userInput.y * hand.Hand.up * MovementSpeed * Time.deltaTime +
+        var handMovement =
+            //userInput.x * hand.Hand.right * hand.UserInputVerticalSpeed * Time.deltaTime +
+            userInput.y * hand.Hand.up * hand.UserInputVerticalSpeed * Time.deltaTime +
             hand.Hand.up * hand.BaseVerticalSpeed * Time.deltaTime +
             hand.Hand.up * UnityEngine.Random.Range(-0.9f, 1) * hand.VerticalShakiness * Time.deltaTime;
         hand.Hand.position += handMovement;
