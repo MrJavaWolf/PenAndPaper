d0d6c0f5294ea2908ef3cf8238764cbea6d6488a
Jens Larsen <jepl@dictus.dk>
2018-04-15 11:26:06 +0200

    Added: Win particle effect

--- Pen And Paper/Assets/Scripts/ChangeChallangeEffect.cs
+++ Pen And Paper/Assets/Scripts/ChangeChallangeEffect.cs
@@ -9,6 +9,7 @@ public class ChangeChallangeEffect : Singleton<ChangeChallangeEffect>
     public Transform RightHand;
     public Transform LeftHand;
     public Transform WinCameraParent;
+    public ParticleSystem WinEffect;
 
 
     public Vector3 CameraOffset = new Vector3(1, 5, 8);
@@ -26,6 +27,8 @@ public class ChangeChallangeEffect : Singleton<ChangeChallangeEffect>
         var paperCentralPosition = (LeftHand.position + RightHand.position) / 2;
         var cameraPosition = paperCentralPosition + CameraOffset;
         var cameraRotation = Quaternion.LookRotation((paperCentralPosition - cameraPosition).normalized, Vector3.up);
+        WinEffect.transform.position = paperCentralPosition + Vector3.forward;
+        WinEffect.Play();
         PlayCamera.Instance.transform.DOMove(cameraPosition, 1).SetEase(Ease.OutCubic);
         PlayCamera.Instance.transform.DORotate(cameraRotation.eulerAngles, 1);
         yield return new WaitForSeconds(0.95f);
@@ -39,6 +42,7 @@ public class ChangeChallangeEffect : Singleton<ChangeChallangeEffect>
         WinCameraParent.DORotateQuaternion(WinCameraParent.rotation * Quaternion.Euler(new Vector3(0, 45, 0)), 5)
             .SetEase(Ease.InOutSine);
         yield return new WaitForSeconds(5);
+        WinEffect.Stop();
         PlaySceneController.Instance.EnablePlayScripts();
         onDone();
     }
